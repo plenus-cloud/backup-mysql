dc6f646554c5bd4a2847763a08bf37fe70d214cd
Massimiliano Ferrero <m.ferrero@cognitio.it>
2018-06-08 13:02:57 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+![Plenus Platform Logo](https://raw.githubusercontent.com/plenus-cloud/backup-mysql/master/docs/img/plenus-platform-logo.png)
+
 # backup-mysql
 Docker container for mysql backup
+
+## Usage
+
+### Docker
+
+    # run every hour command mysqldump --all-databases, mysql is at mysqlhostname:3306
+    docker run -d -e SCHEDULE="* */1 * * *" -e MYSQLPASSWORD=mysecretpassword -e MYSQLHOST=mysqlhostname -e MYSQLUSER=root --name mysql-backup plenus/backup-mysql
+
+    # run every hour command mysqldump for database MYSQLDB,  mysql is at mysqlhostname:33060
+    docker run -d -e SCHEDULE="* */1 * * *" -e MYSQLPASSWORD=mysecretpassword -e MYSQLHOST=mysqlhostname -e MYSQLPORT=33060 -e MYSQLUSER=dbuser -e MYSQLDB=dbname --name mysql-backup plenus/backup-mysql
+
+## Backup location
+
+dump is created in file /dump/mysql.sql.gz (compressed with gzip).
---
+++ docker-entrypoint.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+set -o pipefail
+
+# cron schedule
+# must be in crontab format
+if [ -z "${SCHEDULE}" ]; then
+  echo "env var SCHEDULE must be set in crontab format"
+  echo "Format: minute hour dom month wday"
+  echo "Ex."
+  echo "SCHEDULE='*/5 * * * *'"
+  exit 1
+fi
+
+# CRONCMD: if empty use default command to backup mysql
+# otherwise use this value as backup command, escape \$ to use env vars
+
+# MYSQLDB: db to backup, if empty dump all databases
+
+# mysql hostname, default to mysql
+if [ -z "${MYSQLHOST}" ]; then
+  MYSQLHOST="mysql"
+fi
+
+# mysql port, default to 3306
+if [ -z "${MYSQLPORT}" ]; then
+  MYSQLPORT=3306
+fi
+
+# mysql username, default to root
+if [ -z "${MYSQLUSER}" ]; then
+  MYSQLUSER="root"
+fi
+
+# mysql password
+if [ -z "${MYSQLPASSWORD}" ]; then
+  echo "env MYSQLPASSWORD must contain mysql password"
+  exit 1
+fi
+
+# write cron schedule
+echo "${SCHEDULE}        root    /usr/local/bin/task0.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/task0
+echo "# An empty line is required at the end of this file for a valid cron file." >> /etc/cron.d/task0
+chmod 644 /etc/cron.d/task0
+# write cron job command
+echo "#!/bin/bash" > /usr/local/bin/task0.sh
+echo date >> /usr/local/bin/task0.sh
+echo "echo \"Running \$0\"" >> /usr/local/bin/task0.sh
+echo "echo \"--- BEGIN COMMAND OUTPUT\"" >> /usr/local/bin/task0.sh
+echo "echo \"Cleaning up /dump/ ...\"" >> /usr/local/bin/task0.sh
+echo "find /dump -maxdepth 1 ! -wholename '/dump' ! -wholename '/dump/lost+found' -exec rm -fr {} \;" >> /usr/local/bin/task0.sh
+if [ -z "${CRONCMD}" ]; then
+  if [ -z "${MYSQLDB}" ]; then
+    echo "echo \"Dumping databases...\"" >> /usr/local/bin/task0.sh
+    echo "mysqldump -h ${MYSQLHOST} -P ${MYSQLPORT} -u ${MYSQLUSER} -p\"${MYSQLPASSWORD}\" --all-databases | gzip -c > /dump/mysql.sql.gz" >> /usr/local/bin/task0.sh
+    echo "DUMP_RC=\${PIPESTATUS[0]}" >> /usr/local/bin/task0.sh
+    echo "if [ \${DUMP_RC} -ne 0 ]; then" >> /usr/local/bin/task0.sh
+    echo "  echo \"Error code \${DUMP_RC} in databases dump, retrying dump with -f...\"" >> /usr/local/bin/task0.sh
+    echo "  mysqldump -h ${MYSQLHOST} -P ${MYSQLPORT} -u ${MYSQLUSER} -p\"${MYSQLPASSWORD}\" -f --all-databases | gzip -c > /dump/mysql.sql.gz" >> /usr/local/bin/task0.sh
+    echo "fi" >> /usr/local/bin/task0.sh
+  else
+    echo "echo \"Dumping database ${MYSQLDB}...\"" >> /usr/local/bin/task0.sh
+    echo "mysqldump -h ${MYSQLHOST} -P ${MYSQLPORT} -u ${MYSQLUSER} -p\"${MYSQLPASSWORD}\" ${MYSQLDB} | gzip -c > /dump/mysql.sql.gz" >> /usr/local/bin/task0.sh
+    echo "DUMP_RC=\${PIPESTATUS[0]}" >> /usr/local/bin/task0.sh
+    echo "if [ \${DUMP_RC} -ne 0 ]; then" >> /usr/local/bin/task0.sh
+    echo "  echo \"Error code \${DUMP_RC} in databases dump, retrying dump with -f...\"" >> /usr/local/bin/task0.sh
+    echo "  mysqldump -h ${MYSQLHOST} -P ${MYSQLPORT} -u ${MYSQLUSER} -p\"${MYSQLPASSWORD}\" -f ${MYSQLDB} | gzip -c > /dump/mysql.sql.gz" >> /usr/local/bin/task0.sh
+    echo "fi" >> /usr/local/bin/task0.sh
+  fi
+else
+  echo "${CRONCMD}" >> /usr/local/bin/task0.sh
+fi
+echo "echo \"--- END COMMAND OUTPUT\"" >> /usr/local/bin/task0.sh
+echo "echo \"\"" >> /usr/local/bin/task0.sh
+chmod 755 /usr/local/bin/task0.sh
+
+# Start cron
+cron -L 15
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start cron: $status"
+  exit $status
+fi
+date
+echo "cron started"
+echo ""
+
+# Start logging
+tail -f /var/log/cron.log &
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start logging: $status"
+  exit $status
+fi
+date
+echo "logging started"
+echo ""
+
+# check if processes are alive, if not exit
+while /bin/true; do
+  ps -e -o command | grep "^cron" > /dev/null 2>&1
+  CRON_STATUS=$?
+  ps -e -o command | grep "^tail" > /dev/null 2>&1
+  LOGGING_STATUS=$?
+  # If the greps above find anything, they will exit with 0 status
+  # If they are not both 0, then something is wrong
+  if [ $CRON_STATUS -ne 0 ]; then
+    date
+    echo "cron process terminated, exiting"
+    exit 2
+  fi
+  if [ $LOGGING_STATUS -ne 0 ]; then
+    date
+    echo "logging process terminated, exiting"
+    exit 3
+  fi
+  sleep 20
+done
